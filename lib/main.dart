2f8f52a48d3389bfe7a0655b7853d8fef6c0fed4
BerkayGulen <berkaygulen2@gmail.com>
2023-08-01 13:38:46 +0300

    Default flutter app

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      useMaterial3: true,
+    ),
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Build Test App"),
+        centerTitle: true,
+      ),
+      body: Text("demo"),
+    );
+  }
+}
