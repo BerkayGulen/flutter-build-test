747818784644ba5d572778cdcbe4b16d0388d1c8
admin <admin@admins-MacBook-Pro.local>
2023-08-03 15:28:55 +0300

    responsive loyout page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:build_test_app/PageOne.dart';
 import 'package:build_test_app/PageTwo.dart';
-import 'package:build_test_app/components/BarcodScannerWidget.dart';
-import 'package:build_test_app/components/Layout1.dart';
+import 'package:build_test_app/components/responsive/desktop_body.dart';
+import 'package:build_test_app/components/responsive/mobile_body.dart';
+import 'package:build_test_app/components/responsive/responsive_layout.dart';
 import 'package:flutter/material.dart';
-
-import 'components/Home.dart';
 
 void main() {
   runApp(MaterialApp(
     initialRoute: '/home',
     routes: {
       '/home': (context) => MyApp(),
-
     },
     title: 'Flutter Demo',
     theme: ThemeData(
@@ -26,24 +24,33 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) => DefaultTabController(length: 2, child:  Scaffold(
-    appBar: AppBar(
-      title: Text("Build Test App"),
-      centerTitle: true,
-      bottom: TabBar(
-        tabs: [
-          Tab(text: "page 1",),
-          Tab(text: "page 2",)
-
-        ],
-      ),
-    ),
-    body:TabBarView(
-      children: [
-        PageOne(),
-        PageTwo(),
-      ],
-    ),
-  )
-  );
+  Widget build(BuildContext context) => DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Build Test App"),
+          centerTitle: true,
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                text: "page 1",
+              ),
+              Tab(
+                text: "page 2",
+              ),
+              Tab(
+                text: "page 3 Responsive",
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            PageOne(),
+            PageTwo(),
+            ResponsiveLayout(
+                mobileBody: MyMobileBody(), desktopBody: MyDesktopBody())
+          ],
+        ),
+      ));
 }
